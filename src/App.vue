fa34303730db9eee166b31f056d888305295106d
Rajethan Ranjan <rajethan.ranjan@outlook.com>
2025-09-05 12:30:22 +0200

    Change message

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="IPT 4.1 with VUE" />
 </template>
 
 <style scoped>
